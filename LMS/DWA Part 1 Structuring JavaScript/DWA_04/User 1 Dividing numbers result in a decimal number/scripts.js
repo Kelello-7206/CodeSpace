ec261303da6981646d4e489cfe7b2b8f519eac0e
Kelello-7206 <tseripe7@gmail.com>
2023-06-05 11:25:00 +0200

    added html code

---
+++ LMS/DWA Part 1 Structuring JavaScript/DWA_04/User 1 Dividing numbers result in a decimal number/scripts.js
@@ -0,0 +1,14 @@
+// scripts.js
+
+const form = document.querySelector("[data-form]");
+const result = document.querySelector("[data-result]");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const entries = new FormData(event.target);
+  const { dividend, divider } = Object.fromEntries(entries);
+
+  // Add Math.trunc to remove the decimal places
+  result.innerText = Math.trunc(dividend / divider);
+
+});
