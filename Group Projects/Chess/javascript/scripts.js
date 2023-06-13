f6d60a2b2ece23a31e3b039c8a90845f45c153e3
Kelello-7206 <tseripe7@gmail.com>
2023-06-13 15:58:19 +0200

    added function

--- Group Projects/Chess/javascript/scripts.js
+++ Group Projects/Chess/javascript/scripts.js
@@ -4,6 +4,8 @@ const gameBoard = document.querySelector("#gameboard")
 const playerDisplay = document.querySelector("#player")
 const infoDisplay = document.querySelector("#info-display")
 const width = 8
+let playerGo = 'black'
+playerDisplay.textContent = 'black'
 
 const startPieces = [
 rook, knight, bishop, queen, king, bishop,  knight, rook,
@@ -19,12 +21,14 @@ rook, knight, bishop, queen, king, bishop,  knight, rook
 
 const createBoard = () => {
   startPieces.forEach((startPiece, i) => {
+
     const square = document.createElement("div");
     square.classList.add("square");
     square.innerHTML = startPiece;
     square.firstChild?.setAttribute("draggable", true);
     square.setAttribute("square-id", i);
     square.classList.add("brown");
+    
     const row = Math.floor((63 - i) / 8) + 1;
     if (row % 2 === 0) {
       square.classList.add(i % 2 === 0 ? "brown" : "grey");
@@ -46,26 +50,96 @@ const createBoard = () => {
 createBoard();
 
 
-const squareAll = document.querySelectorAll("#gameboard .square");
+const squareAll = document.querySelectorAll(".square");
 
-let startPostitonId 
-let draggableElement
+
+let startPostitonId;
+let draggedElement;
+
+const changePlayer = () =>{
+if(playerGo === 'black'){
+    reverseIds()
+    playerGo = "white"
+    playerDisplay.textContent = "white"
+}else{
+    revertIds()
+    playerGo = "black"
+    playerDisplay.textContent = "black"
+}
+}
 
 const dragStart = (e) => {
- startPostitonId = console.log(e.target.parentNode.getAttribute('square-id'));
- draggableElement = e.target
+  startPostitonId = e.target.parentNode.getAttribute("square-id");
+  draggedElement = e.target;
 };
 
 const dragOver = (e) => {
-    e.preventDefault()
-}
+  e.preventDefault();
+
+};
 
 const dragDrop = (e) => {
-    e.preventDefault()
+  e.stopPropagation();
+ const correctGo = draggedElement.firstChild.classList.contains(playerGo )
+  const taken = e.target.classList.contains("piece");
+  const valid = checkIfValid(e.target)
+  const opponentGo = playerGo === 'white' ? 'black' : 'white'
+  const takenByOpponent = e.target.firstChild?.classList.contains(opponentGo)
+  
+
+  if (correctGo){
+    if(takenByOpponent && valid){
+       e.target.parentNode.append(draggedElement)
+       e.target.remove() 
+
+       changePlayer()
+        return
+    }
+
+    if(taken && !takenByOpponent){
+        infoDisplay.textContent = 'cannot go here'
+        setTimeout(()=> infoDisplay.textContent = "",2000)
+        return
+    }
+
+    if(valid){
+        e.target.parentNode.append(draggedElement)
+        changePlayer()
+        return
+    }
+  }
+
+  checkIfValid = (target) =>{
+    console.log(target)
+  }
+
+
+  //e.target.append(draggedElement)
+  //e.target.parentNode.append(draggedElement);
+  //e.target.remove();
+
+  changePlayer();
+};
+
+const reverseIds = () =>{
+ const squareAll = document.querySelectorAll(".square")
+
+ squareAll.forEach((square, i) => {
+   square.setAttribute('square-id', (width * width - 1) - i)
+  });
 }
+
+const revertIds = () =>{
+    const squareAll = document.querySelectorAll(".square")
+    
+    squareAll.forEach((square, i) => {
+      square.setAttribute('square-id', i)
+     });
+   }
+   
 
 squareAll.forEach((square) => {
   square.addEventListener("dragstart", dragStart);
-  square.addEventListener("dragover", dragOver );
-  square.addEventListener("drop", dragDrop  );
+  square.addEventListener("dragover", dragOver);
+  square.addEventListener("drop", dragDrop);
 });
