8e8d9339bf5a1ee1fa915b27eea13e4fb1769437
Kelello-7206 <tseripe7@gmail.com>
2023-06-28 09:19:57 +0200

    added redux

--- LMS/DWA Part 1 Structuring JavaScript/DWA_11/JavaScript/component.js
+++ LMS/DWA Part 1 Structuring JavaScript/DWA_11/JavaScript/component.js
@@ -1,53 +1,156 @@
-this.numberInput = this.shadowRoot.querySelector('[data-key="number"]');
-this.subtractButton = this.shadowRoot.querySelector('[data-key="subtract"]');
-this.addButton = this.shadowRoot.querySelector('[data-key="add"]');
-this.resetButton = this.shadowRoot.querySelector('[data-key="reset"]');
+class TallyCount extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.value = 0;
+    this.maxNumber = 12;
+    this.minNumber = -1;
+    this.stepAmount = 1;
+    this.resetValue = 0;
+  }
 
-this.count = 0;
+  connectedCallback() {
+    this.render();
+    this.addEventListeners();
+  }
 
-this.subtractHandler = () => {
-  this.count -= 5;
-  this.updateCountValue();
-  this.updateButtonStates();
-};
+  render() {
+    this.shadowRoot.innerHTML = `
+      <style>
+        /* Controls */
+        .controls {
+          background: yellow;
+        }
 
-this.addHandler = () => {
-  this.count += 5;
-  this.updateCountValue();
-  this.updateButtonStates();
-};
+        /* Counter */
+        .counter {
+          background: var(--color-dark-grey);
+        }
 
-this.resetHandler = () => {
-  this.count = 0;
-  this.updateCountValue();
-  this.updateButtonStates();
-  alert("The counter has been reset");
-};
+        /* Displays the value of the counter */
+        .counter_value {
+          width: 100%;
+          height: 15rem;
+          text-align: center;
+          font-size: 6rem;
+          font-weight: 900;
+          color: var(--color-white);
+          background: none;
+          border-width: 0;
+          border-bottom: 1px solid var(--color-light-grey);
+        }
 
+        sl-button.counter_actions::part(base) {
+          display: flex;
+        }
 
-connectedCallback() {
-this.subtractButton.addEventListener("click", this.subtractHandler);
-this.addButton.addEventListener("click", this.addHandler);
-this.resetButton.addEventListener("click", this.resetHandler);
+        .counter_actions {
+          display: flex;
+        }
 
-this.updateCountValue();
-this.updateButtonStates();
+        sl-button.counter_button::part(base) {
+          background: none;
+          width: 50%;
+          border-width: 0;
+          color: var(--color-white);
+          font-size: 3rem;
+          height: 10rem;
+          border-bottom: 1px solid var(--color-light-grey);
+          /* Has an effect when button is clicked */
+          transition: transform 0.1s;
+          transform: translateY(0);
+        }
+
+        .counter_button {
+          background: none;
+          width: 50%;
+          border-width: 0;
+          color: var(--color-white);
+          font-size: 3rem;
+          height: 10rem;
+          border-bottom: 1px solid var(--color-light-grey);
+          /* Has an effect when button is clicked */
+          transition: transform 0.1s;
+          transform: translateY(0);
+        }
+
+        .counter_button:disabled {
+          opacity: 0.2;
+          /* When the set limit is reached, the button disables */
+        }
+
+        .counter_button:active {
+          background: var(--color-medium-grey);
+          /* Button turns changes color time its clicked */
+        }
+
+        .counter_button_first {
+          border-right: 1px solid var(--color-light-grey);
+        }
+      </style>
+
+      <header class="header">
+        <h1 class="header_title">Tally count</h1>
+      </header>
+      <main class="counter">
+        <input class="counter_value" readonly value="${this.value}" />
+        <div class="counter_actions">
+          <button data-key="subtract" class="counter_button counter_button_first">-</button>
+          <button data-key="add" class="counter_button">+</button>
+          <sl-button data-key="reset" style="height: 10rem; width: 50%; border-bottom: 1px solid; font-size: 3rem; display: flex; transition: transform 0.1s;">Reset</sl-button>
+        </div>
+      </main>
+    `;
+  }
+
+  addEventListeners() {
+    const subtract = this.shadowRoot.querySelector('[data-key="subtract"]');
+    const add = this.shadowRoot.querySelector('[data-key="add"]');
+    const reset = this.shadowRoot.querySelector('[data-key="reset"]');
+    const number = this.shadowRoot.querySelector('.counter_value');
+
+    const subtractHandler = () => {
+      const newValue = parseInt(number.value) - this.stepAmount;
+      number.value = newValue;
+
+      if (add.disabled === true) {
+        add.disabled = false;
+      }
+
+      if (newValue <= this.minNumber) {
+        subtract.disabled = true;
+      }
+    };
+
+    const addHandler = () => {
+      const newValue = parseInt(number.value) + this.stepAmount;
+      number.value = newValue;
+
+      if (subtract.disabled === true) {
+        subtract.disabled = false;
+      }
+
+      if (newValue >= this.maxNumber) {
+        add.disabled = true;
+      }
+    };
+
+    const resetHandler = () => {
+      const newValue = parseInt(this.resetValue);
+      number.value = newValue;
+
+      for (let i = number.value; i <= 10; i++) {
+        if (number.value <= 10) {
+          alert("The counter has been reset");
+        }
+        break;
+      }
+    };
+
+    subtract.addEventListener('click', subtractHandler);
+    add.addEventListener('click', addHandler);
+    reset.addEventListener('click', resetHandler);
+  }
 }
 
-disconnectedCallback() {
-this.subtractButton.removeEventListener("click", this.subtractHandler);
-this.addButton.removeEventListener("click", this.addHandler);
-this.resetButton.removeEventListener("click", this.resetHandler);
-}
-
-updateCountValue() {
-this.numberInput.value = this.count;
-}
-
-updateButtonStates() {
-this.subtractButton.disabled = this.count <= -5;
-this.addButton.disabled = this.count >= 12;
-}
-
-
-customElements.define("tally-count", TallyCount);
+customElements.define('tally-count', TallyCount);
--- LMS/DWA Part 1 Structuring JavaScript/DWA_11/JavaScript/scripts.js
+++ LMS/DWA Part 1 Structuring JavaScript/DWA_11/JavaScript/scripts.js
@@ -1,44 +1,102 @@
-/*NOTE used prettier extension to format the code*/
+// /*NOTE used prettier extension to format the code*/
 
-const MAX_NUMBER = 15;
-const MIN_NUMBER = -10;
-const STEP_AMOUNT = 5;
+// const MAX_NUMBER = 15;
+// const MIN_NUMBER = -10;
+// const STEP_AMOUNT = 5;
 
-/*Global constants set at the top and never change*/
+// /*Global constants set at the top and never change*/
 
-const number = document.querySelector('[data-key="number"]');
-const subtract = document.querySelector('[data-key="subtract"]');
-const add = document.querySelector('[data-key="add"]');
+// const number = document.querySelector('[data-key="number"]');
+// const subtract = document.querySelector('[data-key="subtract"]');
+// const add = document.querySelector('[data-key="add"]');
 
-const subtractHandler = () => {
-  const newValue = parseInt(number.value) - STEP_AMOUNT;
-  number.value = newValue;
+// const subtractHandler = () => {
+//   const newValue = parseInt(number.value) - STEP_AMOUNT;
+//   number.value = newValue;
 
-  if (add.disabled === true) {
-    add.disabled = false;
+//   if (add.disabled === true) {
+//     add.disabled = false;
+//   }
+
+//   if (newValue <= MIN_NUMBER) {
+//     subtract.disabled = true;
+//   }
+// };
+
+//   /*parseInt treats the value as a number not a character*/
+
+//   const addHandler = () => {
+//     const newValue = parseInt(number.value) + STEP_AMOUNT;
+//     number.value = newValue;
+
+//     if (subtract.disabled === true) {
+//       subtract.disabled = false;
+//     }
+
+//     if (newValue >= MAX_NUMBER) {
+//       add.disabled = true;
+//     }
+//   };
+
+// /*addEventListener listens for the event and then does something*/
+// subtract.addEventListener("click", subtractHandler);
+
+// add.addEventListener("click", addHandler);
+
+
+// Actions
+const ADD = 'ADD';
+const SUBTRACT = 'SUBTRACT';
+const RESET = 'RESET';
+
+// Reducer
+function counterReducer(state = { count: 0 }, action) {
+  switch (action.type) {
+    case ADD:
+      return { count: state.count + 1 };
+    case SUBTRACT:
+      return { count: state.count - 1 };
+    case RESET:
+      return { count: 0 };
+    default:
+      return state;
   }
+}
 
-  if (newValue <= MIN_NUMBER) {
-    subtract.disabled = true;
-  }
-};
+import Redux from 'redux';
 
-  /*parseInt treats the value as a number not a character*/
 
-  const addHandler = () => {
-    const newValue = parseInt(number.value) + STEP_AMOUNT;
-    number.value = newValue;
+// Store
+const store = Redux.createStore(counterReducer);
 
-    if (subtract.disabled === true) {
-      subtract.disabled = false;
-    }
+// Subscriber (Console Logger)
+function consoleLogger() {
+  const newState = store.getState();
+  console.log('New state:', newState);
+}
 
-    if (newValue >= MAX_NUMBER) {
-      add.disabled = true;
-    }
-  };
+// Subscribe the console logger to the store
+store.subscribe(consoleLogger);
 
-/*addEventListener listens for the event and then does something*/
-subtract.addEventListener("click", subtractHandler);
+// Scenario 1: Increment the counter by one
+console.log('Scenario 1:');
+console.log('Initial state:', store.getState()); // Expected: { count: 0 }
 
-add.addEventListener("click", addHandler);
+// Scenario 2: Increment the counter by one
+console.log('Scenario 2:');
+store.dispatch({ type: ADD });
+store.dispatch({ type: ADD });
+// Expected console output:
+// New state: { count: 2 }
+
+// Scenario 3: Increment the counter by one
+console.log('Scenario 3:');
+store.dispatch({ type: SUBTRACT });
+// Expected console output:
+// New state: { count: 1 }
+
+// Scenario 4: Resetting the Tally Counter
+console.log('Scenario 4:');
+store.dispatch({ type: RESET });
+// Expected console output:
+// New state: { count: 0 }
