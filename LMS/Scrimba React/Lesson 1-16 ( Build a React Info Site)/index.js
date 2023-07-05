c8d2c85ef86c25745526eab24b2653f1ce96cb1a
Kelello-7206 <tseripe7@gmail.com>
2023-07-05 09:35:05 +0200

    created custom nav

--- LMS/Scrimba React/Lesson 1-16 ( Build a React Info Site)/index.js
+++ LMS/Scrimba React/Lesson 1-16 ( Build a React Info Site)/index.js
@@ -1,53 +1,91 @@
 
 
-// const navbar = (
-//     <nav>
-//         <h1>Bob's Bistro</h1>
-//         <ul>
-//             <li>Menu</li>
-//             <li>About</li>
-//             <li>Contact</li>
-//         </ul>
-//     </nav>
-// )
-
-// ReactDOM.render(navbar, document.getElementById("root"))
-
-// //ReactDOM.createRoot(navbar, document.getElementById("root"))
+// //Lesson 1
+//ReactDOM.render(<h1>Hello, booi!</h1>, document.getElementById("root"))
+//ReactDOM.render(<p>Hello, My name is Kelello!</p>, document.getElementById("root"))
 
 
-// const page = (
+// //Lesson 2
+//ReactDOM.render(<ul><li>Here to learn React</li><li>First time learning React</li></ul>,document.getElementById('root'))
+
+
+// //Lesson 3 - Create your own custom React component!
+// function Navbar() {
+//     return (
+//         <nav className="navbar navbar-expand-lg navbar-light bg-light">
+//             <a className="navbar-brand" href="#">Navbar</a>
+//             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+//                 <span className="navbar-toggler-icon"></span>
+//             </button>
+
+//             <div className="collapse navbar-collapse" id="navbarSupportedContent">
+//                 <ul className="navbar-nav mr-auto">
+//                 <li className="nav-item active">
+//                     <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+//                 </li>
+//                 <li className="nav-item">
+//                     <a className="nav-link" href="#">Link</a>
+//                 </li>
+//                 <li className="nav-item dropdown">
+//                     <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+//                     Dropdown
+//                     </a>
+//                     <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+//                     <a className="dropdown-item" href="#">Action</a>
+//                     <a className="dropdown-item" href="#">Another action</a>
+//                     <div className="dropdown-divider"></div>
+//                     <a className="dropdown-item" href="#">Something else here</a>
+//                     </div>
+//                 </li>
+//                 <li className="nav-item">
+//                     <a className="nav-link disabled" href="#">Disabled</a>
+//                 </li>
+//                 </ul>
+//                 <form className="form-inline my-2 my-lg-0">
+//                     <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+//                     <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+//                 </form>
+//             </div>
+//         </nav>
+//     )
+// }
+// function Content() {
+//     return ( 
+//         <div>
+//          <h1 className="heading">Job</h1>
+//         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque minima molestias nisi beatae quibusdam optio ipsam!
+//          Sint odit quod commodi dignissimos autem pariatur repudiandae qui natus nam adipisci esse voluptates harum nobis iure at,
+//          perferendis eius laudantium sequi veritatis iusto quas, dolor veniam odio. Nihil velit nostrum numquam libero cumque?</p>
+//         </div>
+//     )
+// }
+
+// ReactDOM.render(
 //     <div>
-//         <h1>My awesome website in React</h1>
-//         <h3>Reasons I love React</h3>
-//         <ol>
-//             <li>It's composable</li>
-//             <li>It's declarative</li>
-//             <li>It's a hireable skill</li>
-//             <li>It's actively maintained by skilled people</li>
-//         </ol>
-//     </div>
+//         <Navbar />
+//         <Content />
+//     </div>, document.getElementById("root")
 // )
 
-// ReactDOM.getElementById("root").append(page)
 
+// //Lesson 4 - recreate the above line of code in vanilla JS by creating and appending an h1
 
+// const h1 = document.createElement("ul")
+// h1.textContent = "This is React"
+// h1.className = "header"
+// document.getElementById("root").append(h1)
 
+// // Lesson 5 - Create a navbar in JSX:
 
-function Page(){
-    return (
-        <div>
-            <lo>
-                <li>It's my first learning react</li>
-                <li>It's interesting so far</li>
-            </lo>
-        </div>
-        
-    )
-}
-
-ReactDOM.render(<Page />,document.getElementById('root'))
-
-
-
+const nav1 = (
+    <nav>
+        <h1>Website</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+ReactDOM.render(nav1,document.getElementById('root'))
 
