1e7dfbc2ac2bb6a03f3fa2766fb8e3053d927328
Kelello-7206 <tseripe7@gmail.com>
2023-07-11 11:33:53 +0200

    added files

--- DWA18/src/main.jsx
+++ DWA18/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from ''
+import { App } from './components/App'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
