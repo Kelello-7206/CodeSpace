51bdbe8d7cb862d441fa23bbe3a35149617bf671
Kelello-7206 <tseripe7@gmail.com>
2023-07-11 11:25:26 +0200

    added new vite project

--- DWA18/src/main.jsx
+++ DWA18/src/main.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from "./components/App"
+import { App } from ''
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
 )
