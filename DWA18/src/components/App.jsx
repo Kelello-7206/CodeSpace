81dbf5afb6ba46c66a0f9c14fdb0452763650410
Kelello-7206 <tseripe7@gmail.com>
2023-08-29 21:52:39 +0200

    default commit

--- DWA18/src/components/App.jsx
+++ DWA18/src/components/App.jsx
@@ -1,3 +1,79 @@
-export const App = () =>{
-    return <div>1234568</div>
+import { useState } from 'react'
+import { v4 as createdID } from 'uuid'
+import React from 'react';
+
+const useTasks  = () =>{
+ const [tasks ,setTasks] = useState([]);
+
+const add = (title) => {
+   const newTasks = 
+   [
+    {
+        id: createdID(),
+        title,
+    },
+    ... tasks,
+   ]
+   setTasks(newTasks);
+};
+
+const remove = (id) =>{
+    const newTask = tasks.filter(item =>{
+        return item.id !== id;
+    });
+
+    setTasks(newTask);
+};
+
+const formSubmit = (event) =>{
+    event.preventDefault();
+    const { target } = event;
+    const data = new FormData(target);
+    const { title } = Object.fromEntries(data);
+    add(title);
+
+    const newTask = {
+        id: createdID(),
+        title,
+
+    };
+
+    setTasks([title, ... tasks]);
+
+   target.reset();
 }
+
+ return [
+    tasks,
+    {
+        add,
+        remove,
+        formSubmit,
+    }
+ ]
+}
+
+export const App = () =>{
+    const { tasks, formSubmit } = useTasks();
+    return (
+    <div className='red-button '>
+        <form onSubmit={formSubmit}>
+            <label>
+                <div>New Task</div>
+                <input name='title' />
+            </label>
+
+            <button type='submit'>SAVE</button>
+        </form>
+
+    <ul>
+         {tasks.map(({id,title}) => {
+            return <li key={id}>
+                <span>{title }</span>
+                <button onClick={() => remove(id)}>X</button>
+                </li>;
+        })}
+    </ul>
+    </div>
+    );
+}; 
