dc15154a2431de5be1363931651341294bd8e73c
Kelello-7206 <tseripe7@gmail.com>
2023-09-04 22:56:50 +0200

    Added folders with js files

---
+++ Udemy-next.js/Section4/components/event-item.js
@@ -0,0 +1,29 @@
+import Link from 'next/link'
+
+function EventItem(props) {
+    const { title, image, date, location, id } = props;
+
+    
+
+  return (
+    <li>
+      <img src="" alt="" />
+      <div>
+        <div>
+          <h2>{title}</h2>
+          <div>
+            <time>DATE</time>
+          </div>
+          <div>
+            <address>ADDRESS</address>
+          </div>
+        </div>
+        <div>
+            <Link href='/'>Explore Event</Link>
+        </div>
+      </div>
+    </li>
+  );
+}
+
+export default EventItem;
--- Udemy-next.js/Section4/pages/index.js
+++ Udemy-next.js/Section4/pages/index.js
@@ -1,6 +1,12 @@
+import { getFeaturedEvents } from "../dummy-data";
+
 function HomePage() {
+    const featuredEvents = getFeaturedEvents();
+
     return(
-        <div><h1>Home Page</h1></div>
+        <div>
+           <ul></ul>
+        </div>
     );
 }
 
