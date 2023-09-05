1029eb638ecd6157af9de93e86c57b2128024baa
Kelello-7206 <tseripe7@gmail.com>
2023-09-05 15:35:15 +0200

    Added js and css files

--- Udemy-next.js/Section4/pages/events/[...slug].js
+++ Udemy-next.js/Section4/pages/events/[...slug].js
@@ -1,43 +1,51 @@
 import { useRouter } from "next/router";
 import { getFilteredEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
-
+import { Fragment } from "react";
+import ResultsTitle from "../../components/events/results-title";
 
 function FilteredEventsPage() {
   const router = useRouter();
 
   const filterData = router.query.slug;
 
- if (!filterData) {
-  return <p className="center">Loading...</p>
- }
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
+  }
 
- const filteredYear = filterData[0];
- const filteredMonth = filterData[1];
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
 
- const numYear = + filteredYear;
- const numMonth = + filteredMonth;
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
 
- if(isNaN(numYear) || isNaN(numMonth) || numYear > 2030 || numYear < 2021 || numMonth < 1 || numMonth > 12) {
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p>Invalid filter. Adjust your values!</p>;
+  }
 
-  return <p>Invalid filter. Adjust your values!</p>
- }
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
 
- const filteredEvents = getFilteredEvents({
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p>No events found for the chosen filter!</p>;
+  }
 
-  year: numYear,
-  month: numMonth,
- });
-
-
- if(!filteredEvents || filteredEvents.length === 0) {
-  return <p>No events found for the chosen filter!</p>
- }
+  const date = new Date(numYear, numMonth -1);
 
   return (
-    <div>
-        <EventList items={filteredEvents} />
-    </div>
+    <Fragment>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
+    </Fragment>
   );
 }
 
