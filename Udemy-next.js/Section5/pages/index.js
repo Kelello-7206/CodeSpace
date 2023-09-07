b55ad5c3ca07be4e98fbdd3af460b35afc4fbcea
Kelello-7206 <tseripe7@gmail.com>
2023-09-07 04:12:57 +0200

    Added function

--- Udemy-next.js/Section5/pages/index.js
+++ Udemy-next.js/Section5/pages/index.js
@@ -1,11 +1,42 @@
+import { NOTFOUND } from 'dns';
+import fs from 'fs/promises';
+import path from 'path';
+
 function HomePage(props) {
+  const { products } = props;
+
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+     {products.map((products) => (
+      <li key={products.id}>{products.title}</li>
+     ))}
     </ul>
   );
 }
- 
+
+export async function getStaticProps() {
+  console.log('(Re-)Generating...')
+  const filepath = path.join(process.cwd(), 'data', 'dummy-backend.json')
+  const jsonData = await fs.readFile(filepath);
+  const data = JSON.parse(jsonData);
+
+  if(!data){
+    return { redirect: {
+      destination: 'no/data'
+    }}
+  }
+
+
+  if(data.products.length === 0){
+    return { NOTFOUND: true }
+  }
+
+  return {
+    props: {
+      products: data.products
+    },
+    revalidate: 10,
+  };
+}
+
 export default HomePage;
