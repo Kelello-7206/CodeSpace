d365b7e9615eded12fa21388919e65d439aa3c56
Kelello-7206 <tseripe7@gmail.com>
2023-09-17 19:54:02 +0200

    commit default

--- Udemy-next.js/Section9/pages/api/comments/[eventId].js
+++ Udemy-next.js/Section9/pages/api/comments/[eventId].js
@@ -1,53 +1,57 @@
-import { MongoClient } from "mongodb";
+import { MongoClient } from 'mongodb';
 
 async function handler(req, res) {
   const eventId = req.query.eventId;
 
-  const client = await MongoClient.connect('mongodb+srv://user1:qBRTpLJ0bCKc5jFK@cluster0.s2ffwug.mongodb.net/events?retryWrites=true&w=majority&appName=AtlasApp')
+  const client = await MongoClient.connect(
+    'mongodb+srv://user1:qBRTpLJ0bCKc5jFK@cluster0.s2ffwug.mongodb.net/events?retryWrites=true&w=majority&appName=AtlasApp'
+  );
 
-  if (req.method === "POST") {
+  if (req.method === 'POST') {
     const { email, name, text } = req.body;
 
     if (
-      !email.includes("@") ||
+      !email.includes('@') ||
       !name ||
-      name.trim() === "" ||
+      name.trim() === '' ||
       !text ||
-      text.trim() === ""
+      text.trim() === ''
     ) {
-      res.status(422).json({ message: "Invalid input" });
+      res.status(422).json({ message: 'Invalid input.' });
       return;
     }
-
-    console.log(email, name, text);
 
     const newComment = {
       email,
       name,
       text,
-      eventId
+      eventId,
     };
 
     const db = client.db();
 
-   const result = await db.collection('comments').insertOne(newComment);
+    const result = await db.collection('comments').insertOne(newComment);
 
     console.log(result);
 
-    res.status(201).json({ message: "Added ne comment" , comment: newComment});
+    newComment.id = result.insertedId;
+
+    res.status(201).json({ message: 'Added comment.', comment: newComment });
   }
 
-  if (req.method === "GET") {
-    const dummyList = [
-        {id: 'c1', name:"max", text: " A first comment"},
-        {id: 'c2', name:"james", text: " A second comment"}
+  if (req.method === 'GET') {
+    const db = client.db();
 
-    ]
+    const documents = await db
+      .collection('comments')
+      .find()
+      .sort({ _id: -1 })
+      .toArray();
 
-    res.status(200).json({comments: dummyList});
+    res.status(200).json({ comments: documents });
   }
 
-  client.close
+  client.close();
 }
 
 export default handler;
--- Udemy-next.js/Section9/pages/api/newsletter.js
+++ Udemy-next.js/Section9/pages/api/newsletter.js
@@ -1,24 +1,24 @@
-import { MongoClient } from 'mongodb'
+import { MongoClient } from 'mongodb';
 
 async function handler(req, res) {
-  if (req.method === "POST") {
+  if (req.method === 'POST') {
     const userEmail = req.body.email;
-
-    if (!userEmail || !userEmail.includes("@")) {
-      res.status(442).json({ message: "Invalid email address." });
+    
+    if (!userEmail || !userEmail.includes('@')) {
+      res.status(422).json({ message: 'Invalid email address.' });
       return;
     }
 
-    const client = await MongoClient.connect('mongodb+srv://user1:qBRTpLJ0bCKc5jFK@cluster0.s2ffwug.mongodb.net/events?retryWrites=true&w=majority&appName=AtlasApp'
-    )
+    const client = await MongoClient.connect(
+      'mongodb+srv://user1:qBRTpLJ0bCKc5jFK@cluster0.s2ffwug.mongodb.net/events?retryWrites=true&w=majority&appName=AtlasApp'
+    );
     const db = client.db();
 
     await db.collection('newsletter').insertOne({ email: userEmail });
 
     client.close();
 
-    res.status(201).json({ message: 'Signed up!' })
+    res.status(201).json({ message: 'Signed up!' });
   }
 }
-
 export default handler;
