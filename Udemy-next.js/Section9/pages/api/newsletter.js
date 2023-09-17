9537665a02fd28a396c52a9924dca87213da1a79
Kelello-7206 <tseripe7@gmail.com>
2023-09-17 20:41:40 +0200

    Added error handling

--- Udemy-next.js/Section9/pages/api/newsletter.js
+++ Udemy-next.js/Section9/pages/api/newsletter.js
@@ -1,23 +1,31 @@
-import { MongoClient } from 'mongodb';
+import {connectDatabase, insertDocument} from "../../helpers/db-util"
 
 async function handler(req, res) {
   if (req.method === 'POST') {
     const userEmail = req.body.email;
-    
+
     if (!userEmail || !userEmail.includes('@')) {
       res.status(422).json({ message: 'Invalid email address.' });
       return;
     }
 
-    const client = await MongoClient.connect(
-      'mongodb+srv://user1:qBRTpLJ0bCKc5jFK@cluster0.s2ffwug.mongodb.net/events?retryWrites=true&w=majority&appName=AtlasApp'
-    );
-    const db = client.db();
+    let client;
 
-    await db.collection('newsletter').insertOne({ email: userEmail });
+    try{
+      client = await connectDatabase();
+    }catch (error){
+      res.status(500).json({ message: 'connect to database failed' });
+      return;
+    }
 
+    try{
+      await insertDocument(client, 'newsletter', {email: userEmail})
     client.close();
-
+    }catch (error){
+      res.status(500).json({ message: 'Inserting data failed' });
+      return;
+    }
+   
     res.status(201).json({ message: 'Signed up!' });
   }
 }
