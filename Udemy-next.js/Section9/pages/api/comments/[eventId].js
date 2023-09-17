826b1d61a2fdc1b52e14e41dfb193d041d967ed4
Kelello-7206 <tseripe7@gmail.com>
2023-09-17 17:53:18 +0200

    Added mongodb route

--- Udemy-next.js/Section9/pages/api/comments/[eventId].js
+++ Udemy-next.js/Section9/pages/api/comments/[eventId].js
@@ -1,5 +1,9 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   const eventId = req.query.eventId;
+
+  const client = await MongoClient.connect('mongodb+srv://user1:qBRTpLJ0bCKc5jFK@cluster0.s2ffwug.mongodb.net/events?retryWrites=true&w=majority&appName=AtlasApp')
 
   if (req.method === "POST") {
     const { email, name, text } = req.body;
@@ -18,14 +22,17 @@ function handler(req, res) {
     console.log(email, name, text);
 
     const newComment = {
-      id: new Date().toISOString(),
       email,
       name,
       text,
+      eventId
     };
 
-    console.log(newComment);
+    const db = client.db();
 
+   const result = await db.collection('comments').insertOne(newComment);
+
+    console.log(result);
 
     res.status(201).json({ message: "Added ne comment" , comment: newComment});
   }
@@ -39,6 +46,8 @@ function handler(req, res) {
 
     res.status(200).json({comments: dummyList});
   }
+
+  client.close
 }
 
 export default handler;
--- Udemy-next.js/Section9/pages/api/newsletter.js
+++ Udemy-next.js/Section9/pages/api/newsletter.js
@@ -9,11 +9,11 @@ async function handler(req, res) {
       return;
     }
 
-    const client = await MongoClient.connect('mongodb+srv://user1:qBRTpLJ0bCKc5jFK@cluster0.s2ffwug.mongodb.net/newsletter?retryWrites=true&w=majority&appName=AtlasApp'
+    const client = await MongoClient.connect('mongodb+srv://user1:qBRTpLJ0bCKc5jFK@cluster0.s2ffwug.mongodb.net/events?retryWrites=true&w=majority&appName=AtlasApp'
     )
     const db = client.db();
 
-    await db.collection('emails').insertOne({ email: userEmail });
+    await db.collection('newsletter').insertOne({ email: userEmail });
 
     client.close();
 
