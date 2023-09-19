ba2b75d5f8f1c38d41fdce44b0749426ce465f4a
Kelello-7206 <tseripe7@gmail.com>
2023-09-19 10:27:20 +0200

    Added image

--- Udemy-next.js/Section11/components/home-page/featured-post.js
+++ Udemy-next.js/Section11/components/home-page/featured-post.js
@@ -1,9 +1,10 @@
+import PostItem from '../posts/post-item';
 import classes from './featured-posts.module.css'
 
-function FeaturedPosts() {
+function FeaturedPosts(props) {
   return <section className={classes.latest}>
     <h2>Featured Posts</h2>
-
+    <PostItem posts={props.posts} />
   </section>
 }
 
--- Udemy-next.js/Section11/pages/index.js
+++ Udemy-next.js/Section11/pages/index.js
@@ -1,10 +1,16 @@
+import FeaturedPosts from '../components/home-page/featured-post';
 import Hero from '../components/home-page/hero';
 import { Fragment } from 'react'
+
+const DUMMY_POSTS = [
+    { title, image, excerpt, date, slug }
+];
 
 function Homepage() {
     return (
         <Fragment>
             <Hero />
+            <FeaturedPosts posts={DUMMY_POSTS} />
         </Fragment>
     )
 }
