401b7767e48dcd87871ed9e72e15cefafa41486b
Kelello-7206 <tseripe7@gmail.com>
2023-09-20 15:25:27 +0200

    Using Markdown Data For Rendering Posts

--- Udemy-next.js/Section11/lib/posts-util.js
+++ Udemy-next.js/Section11/lib/posts-util.js
@@ -2,31 +2,30 @@ import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
 
-const postsDirectory = path.join(process.cwd(), 'post');
+const postsDirectory = path.join(process.cwd(), 'posts');
 
 function getPostData(fileName) {
   const filePath = path.join(postsDirectory, fileName);
-  const fileContent = fs.readdirSync(filePath, 'utf-8');
+  const fileContent = fs.readFileSync(filePath, 'utf-8');
+  const { data, content } = matter(fileContent);
 
-  const { data, content } = matter(fileContent)
-
-  const postSlug = fileName.replace(/\.md$/, ''); //removes the file extension
+  const postSlug = fileName.replace(/\.md$/, ''); // removes the file extension
 
   const postData = {
     slug: postSlug,
     ...data,
     content,
-  }
+  };
 
-  return postData
+  return postData;
 }
 
 export function getAllPosts() {
   const postFiles = fs.readdirSync(postsDirectory);
 
   const allPosts = postFiles.map(postFile => {
-    return getPostData(postFile)
-  })
+    return getPostData(postFile);
+  });
 
   const sortedPosts = allPosts.sort((postA, postB) => postA.date > postB.date ? -1 : 1);
 
@@ -40,4 +39,3 @@ export function getFeaturedPosts() {
 
   return featuredPosts;
 }
-
--- Udemy-next.js/Section11/posts/getting-started-with-nextjs.md
+++ Udemy-next.js/Section11/posts/getting-started-with-nextjs.md
@@ -1,8 +1,8 @@
 ---
- title: 'Getting Started with NextJS',
- date: '2022-02-10',
- image: 'getting-started-nextjs.png',
- excerpt: 'NextJS is a the React framework production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
+ title: 'Getting Started with NextJS'
+ date: '2022-02-18'
+ image: 'getting-started-nextjs.png'
+ excerpt: 'NextJS is a the React framework production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.'
  isFeatured: true
 ---
 
