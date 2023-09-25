6dbce0e61a7930d3d1bdd2621ab9924d05214ef4
Kelello-7206 <tseripe7@gmail.com>
2023-09-25 07:39:27 +0200

    Storing Messages With MongoDB In A Database

--- Udemy-next.js/Section11/pages/api/contact.js
+++ Udemy-next.js/Section11/pages/api/contact.js
@@ -1,17 +1,17 @@
+import { MongoClient } from 'mongodb'
 
-
-function handler(req, res) {
+async function handler(req, res) {
   const { email, name, message } = req.body;
 
   if (!email ||
-      !email.includes('@') ||
-      !name ||
-      !name.trim() === "" ||
-      !message ||
-      !message.trim() === ""
-    ) {
-      res.status(402).json({message: 'Invalid input'})
-      return;
+    !email.includes('@') ||
+    !name ||
+    !name.trim() === "" ||
+    !message ||
+    !message.trim() === ""
+  ) {
+    res.status(402).json({ message: 'Invalid input' })
+    return;
   }
 
   const newMessage = {
@@ -20,10 +20,31 @@ function handler(req, res) {
     message,
   };
 
-  console.log(newMessage)
+  let client;
 
-  res.status(201).json({message: 'Success stored message' , message: newMessage})
+  try {
 
+    client = await MongoClient.connect('mongodb+srv://user3:xpIOagry1PEc0tLT@cluster0.s2ffwug.mongodb.net/my-site?retryWrites=true&w=majority&appName=AtlasApp'
+    )
+
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to connect to database' })
+    return
+  }
+
+  const db = client.db();
+
+  try {
+    const result = await db.collection('messages').insertOne(newMessage)
+    newMessage.id = result.insertedId;
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to store message' })
+    return
+  }
+
+  client.close()
+
+  res.status(201).json({ message: 'Success stored message', message: newMessage })
 }
 
 export default handler
