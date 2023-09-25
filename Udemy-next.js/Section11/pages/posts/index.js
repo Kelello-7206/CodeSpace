a5fc35623c38d5fad6734b182658830230665484
Kelello-7206 <tseripe7@gmail.com>
2023-09-25 10:35:24 +0200

    Added head data

--- Udemy-next.js/Section11/pages/posts/index.js
+++ Udemy-next.js/Section11/pages/posts/index.js
@@ -1,9 +1,19 @@
+import { Fragment } from "react";
 import AllPosts from "../../components/posts/all-posts";
 import { getAllPosts } from "../../lib/posts-util";
+import Head from "next/head";
 
 
 function AllPostPage(props) {
-  return <AllPosts posts={props.posts}/>
+  return (
+    <Fragment>
+      <Head>
+        <title>All posts</title>
+        <meta name='description' content='List of posts' />
+      </Head>
+      <AllPosts posts={props.posts} />
+    </Fragment>
+  )
 }
 
 export function getStaticProps() {
